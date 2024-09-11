43f3bd8fa451a7ff167c3a0a519f5bcdc8c30a1e
Firdavsbek Ismoilov <firdavsbek_ismoilov@epam.com>
2024-09-11 15:58:34 +0500

    Initial commit: lab3

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeedRaise;
+    [SerializeField] private ParticleSystem explosionEffect;
+
+    private float movementSpeedX;
+    private float movementSpeedY;
+
+    private float minBaseSpeed = 5f;
+    private float maxBaseSpeed = 7f;
+
+    void Update()
+    {
+        transform.Translate(transform.right * movementSpeedX * Time.deltaTime + transform.up * movementSpeedY * Time.deltaTime);
+    }
+
+    public void AddSpeed()
+    {
+        movementSpeedX = (Random.Range(0, 2) * 2 - 1) * Random.Range(minBaseSpeed, maxBaseSpeed);
+        movementSpeedY = (Random.Range(0, 2) * 2 - 1) * Random.Range(minBaseSpeed, maxBaseSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Paddle")
+        {
+            movementSpeedX = -(movementSpeedX + Mathf.Sign(movementSpeedX) * movementSpeedRaise);
+            movementSpeedY = movementSpeedY + Mathf.Sign(movementSpeedY) * movementSpeedRaise;
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            movementSpeedY = -movementSpeedY;
+        }
+        else if (collision.gameObject.tag == "LeftGoal")
+        {
+            movementSpeedX = 0;
+            movementSpeedY = 0;
+            CreateExplosion();
+            GameManager.GetInstance().IncreasePlayerTwoScore();
+        }
+        else if (collision.gameObject.tag == "RightGoal")
+        {
+            movementSpeedX = 0;
+            movementSpeedY = 0;
+            CreateExplosion();
+            GameManager.GetInstance().IncreasePlayerOneScore();
+        }
+    }
+
+    private void CreateExplosion()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        explosionEffect.Play();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private bool isPlayerTwo;
+    [SerializeField] private Transform ball;
+
+    private float maximumYPosition = 4.2f;
+    private Vector2 movementVector;
+
+    private bool isVSPlayer;
+
+    private void Start()
+    {
+        isVSPlayer = GameManager.GetInstance().isVSPlayer;
+        if (isPlayerTwo)
+        {
+            if (isVSPlayer)
+            {
+                GetComponent<SpriteRenderer>().color = new Color(112f / 255f, 187f / 255f, 1);
+            }
+            else
+            {
+                movementSpeed = 6f;
+            }
+        }
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        movementVector = context.ReadValue<Vector2>().normalized;
+    }
+
+    void Update()
+    {
+        float deltaY;
+        if (!isVSPlayer && isPlayerTwo)
+        {
+            deltaY = Mathf.Sign(ball.transform.position.y - transform.position.y) * movementSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + deltaY, -maximumYPosition, maximumYPosition), transform.position.z);
+        }
+        else
+        {
+            deltaY = movementVector.y * movementSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + deltaY, -maximumYPosition, maximumYPosition), transform.position.z);
+        }
+    }
+}
