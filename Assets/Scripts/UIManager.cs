84d846a06dd7cfe34fb4fa4f15f0a81c33d6e0b3
Firdavsbek Ismoilov <firdavsbek_ismoilov@epam.com>
2024-09-16 23:16:50 +0500

    Configure UI, add GameStart, GameOver scenes

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UIManager : Singleton<UIManager>
+{
+    private List<UIController> UIControllers;
+    private UIController lastActiveUI;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        UIControllers = GetComponentsInChildren<UIController>().ToList();
+        UIControllers.ForEach(x => x.gameObject.SetActive(false));
+        SwitchUI(UIType.MainMenu);
+    }
+
+    public void SwitchUI(UIType type)
+    {
+        if (lastActiveUI != null)
+        {
+            lastActiveUI.gameObject.SetActive(false);
+        }
+        UIController desiredUI = UIControllers.Find(x => x.canvasType == type);
+        if (desiredUI == null)
+        {
+            Debug.LogWarning("The desired UI was not found!");
+            return;
+        }
+        desiredUI.gameObject.SetActive(true);
+        lastActiveUI = desiredUI;
+    }
+
+    public void ActiveteObject(UIType type, string name, bool isActive)
+    {
+        UIController desiredUI = UIControllers.Find(x => x.canvasType == type);
+        if (desiredUI == null)
+        {
+            Debug.LogWarning("The desired UI was not found!");
+            return;
+        }
+        Transform objectTransform = desiredUI.gameObject.transform.Find(name);
+        if (objectTransform == null)
+        {
+            Debug.LogWarning("The desired object was not found!");
+            return;
+        }
+        objectTransform.gameObject.SetActive(isActive);
+    }
+
+    public void ChangeText(UIType type, string name, string text)
+    {
+        UIController desiredUI = UIControllers.Find(x => x.canvasType == type);
+        if (desiredUI == null)
+        {
+            Debug.LogWarning("The desired UI was not found!");
+            return;
+        }
+        Transform objectTransform = desiredUI.gameObject.transform.Find(name);
+        if (objectTransform == null)
+        {
+            Debug.LogWarning(name + " was not found!");
+            return;
+        }
+        Text objectText = objectTransform.gameObject.GetComponent<Text>();
+        if (objectText == null)
+        {
+            Debug.LogWarning(name + " was not found!");
+            return;
+        }
+        objectTransform.gameObject.GetComponent<Text>().text = text;
+    }
+
+    public void ShowModeMenu()
+    {
+        SwitchUI(UIType.ModeMenu);
+    }
+}
---
+++ Assets/Scripts/UIType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIType
+{
+    MainMenu,
+    ModeMenu,
+    Gameplay,
+    GameOver
+}
